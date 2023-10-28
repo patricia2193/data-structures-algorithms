76ac48d5b9b967505a3d6d60ee38739a95b658fe
patricia.petruta <patricia.petruta@mandg.com>
2023-10-28 11:23:37 +0300

    fizz buzz solution

---
+++ session1-introduction/src/FizzBuzz.java
@@ -0,0 +1,28 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+
+    public static void getFizzBuzzAnswer(int n) {
+        List<String> result = new ArrayList<>();
+
+        for(int i=1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0){
+                result.add("Fizz");
+            } else if (i % 5 ==0) {
+                result.add("Buzz");
+            } else {
+                result.add(i + "");
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        getFizzBuzzAnswer(100);
+    }
+}
